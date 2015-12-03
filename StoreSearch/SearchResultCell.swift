00ebb3ec45a51ebd7f9b7e37f769f83172f0e912
Molly Waggett <mwaggett@alumni.reed.edu>
2015-12-02 16:54:03 -0800

    Style search bar and add icon.

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -15,8 +15,10 @@ class SearchResultCell: UITableViewCell {
   @IBOutlet weak var artworkImageView: UIImageView!
 
     override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+      super.awakeFromNib()
+      let selectedView = UIView(frame: CGRect.zero)
+      selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 0.5)
+      selectedBackgroundView = selectedView
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
